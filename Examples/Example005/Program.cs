1442bb91825e8dac304a1479c8d2032bf902b6ee
Yuriy Isabaev <isabaevyy@gmail.com>
2022-05-25 12:15:33 +0300

    example5

---
+++ Examples/Example005/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() =="masha")
+{
+    Console.WriteLine("URA eto ZHE MasHa");
+}
+else
+{
+    Console.Write("PRIVET, ");
+    Console.WriteLine(username);
+
+
+}
